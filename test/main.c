4f6aba8a0da9fe4c4983f8cb673176a90375299d
Elias ZANOTTI <zanotti.elias@gmail.com>
2025-01-09 12:10:29 +0100

    fix main.c

--- test/main.c
+++ test/main.c
@@ -7,13 +7,13 @@ extern size_t ft_strlen(const char *str);
 void strlen_print_result(const char *str) {
     size_t ft_strlen_result = ft_strlen(str);
     size_t strlen_result = strlen(str);
-    printf("\n--- %s ---\n", str);
+    printf("--- %s ---\n", str);
     printf("ft_strlen : %zu\n", ft_strlen_result);
     printf("strlen    : %zu\n", strlen_result);
     if (ft_strlen_result == strlen_result) {
-        printf("\033[0;32mOK\033[0m\n");
+        printf("\033[0;32mOK\033[0m\n\n");
     } else {
-        printf("\033[0;31mFAILED\033[0m\n");
+        printf("\033[0;31mFAILED\033[0m\n\n");
     }
 }
 
@@ -25,6 +25,7 @@ void test_strlen() {
         "1234567890",
         "This is a longer string to test the length function.",
         "Special characters: !@#$%^&*()_+",
+        "こんにちは",
         NULL
     };
 
@@ -38,12 +39,10 @@ void test_strlen() {
     }
     long_str[1000] = '\0';
     strlen_print_result(long_str);
-
-    char *unicode_str = "こんにちは";
-    strlen_print_result(unicode_str);
 }
 
 int main() {
+    printf("\033[0;34mSTRLEN TEST\033[0m\n");
 	test_strlen();
     return 0;
 }
