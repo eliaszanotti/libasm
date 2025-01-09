f6283305b80d5e1f0ee17e5199e3c58aa232ee02
Elias ZANOTTI <zanotti.elias@gmail.com>
2025-01-09 14:37:46 +0100

    fix some main

--- test/main.c
+++ test/main.c
@@ -3,6 +3,7 @@
 void strlen_print_result(const char *str) {
     size_t ft_strlen_result = ft_strlen(str);
     size_t strlen_result = strlen(str);
+
     printf("--- %s ---\n", str);
     printf("ft_strlen : %zu\n", ft_strlen_result);
     printf("strlen    : %zu\n", strlen_result);
@@ -37,33 +38,42 @@ void test_strlen() {
     strlen_print_result(long_str);
 }
 
-void strcpy_print_result(const char *src) {
-    char strcpy_result[1000];
+void check_strcpy_result(const char *src, const char *expected) {
     char ft_strcpy_result[1000];
+    char strcpy_result[1000];
     ft_strcpy(ft_strcpy_result, src);
     strcpy(strcpy_result, src);
+
     printf("--- %s ---\n", src);
     printf("ft_strcpy : %s\n", ft_strcpy_result);
-    printf("strcpy    : %s\n\n", strcpy_result);
+    printf("strcpy    : %s\n", strcpy_result);
+    if (strcmp(ft_strcpy_result, expected) == 0 && strcmp(strcpy_result, expected) == 0) {
+        printf("\033[0;32mOK\033[0m\n\n");
+    } else {
+        printf("\033[0;31mFAILED\033[0m\n\n");
+    }
 }
 
-void test_strcpy() {
-    char *srcs[] = {
+void test_strcpy(void) {
+    char *strs[] = {
         "Hello, world!",
         "",
         "A",
         "1234567890",
+        "This is a longer string to test the copy function.",
+        "Special characters: !@#$%^&*()_+",
+        "こんにちは",
         NULL
     };
 
-    for (int i = 0; srcs[i] != NULL; i++) {
-        strcpy_print_result(srcs[i]);
+    for (int i = 0; strs[i] != NULL; i++) {
+        check_strcpy_result(strs[i], strs[i]);
     }
 }
 
 int main() {
     printf("\033[0;34mSTRLEN TEST\033[0m\n");
-	test_strlen();
+    test_strlen();
     printf("\033[0;34mSTRCPY TEST\033[0m\n");
     test_strcpy();
     return 0;
