1a24f7b1e653c6371c098936f1fd3d7ca300ff93
Elias ZANOTTI <zanotti.elias@gmail.com>
2025-01-13 11:07:27 +0100

    fix ft_write

--- src/ft_write.s
+++ src/ft_write.s
@@ -1,10 +1,10 @@
 global ft_write
+extern	__errno_location
 
 section .text
 	ft_write:
 		cmp rdi, 0
 		jl .error
-
 
 		mov rax, 1
 		syscall
@@ -14,5 +14,9 @@ section .text
 		ret
 
 	.error:
+		neg rax ; pas necessaire, a tester
+		mov rdi, rax
+		call __errno_location wrt ..plt
+		mov rax, rdi
 		mov rax, -1
 		ret
