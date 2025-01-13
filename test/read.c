5c336bab23ae93ab9974daeb6025057e4baa061b
Elias ZANOTTI <zanotti.elias@gmail.com>
2025-01-13 15:52:01 +0100

    add test for read

--- test/read.c
+++ test/read.c
@@ -4,17 +4,19 @@ static void _read_print_result(int fd, char *buffer, size_t len) {
     ssize_t ft_read_result;
     ssize_t read_result;
 
+    lseek(fd, 0, SEEK_SET);
     errno = 0;
     ft_read_result = ft_read(fd, buffer, len);
     int ft_errno = errno;
 
+    lseek(fd, 0, SEEK_SET);
     errno = 0;
     read_result = read(fd, buffer, len);
     int std_errno = errno;
 
-    printf("--- Buffer: \"%.*s\" ---\n", (int)len, buffer);
-    printf("ft_read  : %zd (errno: %d)\n", ft_read_result, ft_errno);
-    printf("read     : %zd (errno: %d)\n", read_result, std_errno);
+    printf("--- test_file ---\n");
+    printf("ft_read  : buffer = %s | %zd (errno: %d)\n", buffer, ft_read_result, ft_errno);
+    printf("read     : buffer = %s | %zd (errno: %d)\n", buffer, read_result, std_errno);
     if (ft_read_result == read_result && ft_errno == std_errno) {
         printf("\033[0;32mOK\033[0m\n\n");
     } else {
@@ -25,7 +27,7 @@ static void _read_print_result(int fd, char *buffer, size_t len) {
 void test_read() {
     char buffer[100];
     int fd = open("test_file", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    write(fd, "Hello, world!\nThis is a test file.\n", 36);
+    write(fd, "Hello, world! This is a test file.", 35);
     close(fd);
 
     fd = open("test_file", O_RDONLY);
