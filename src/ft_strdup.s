575a1a21848e5da398e4dc57934a2efc610e58b0
Elias ZANOTTI <zanotti.elias@gmail.com>
2025-01-14 11:21:16 +0100

    fix ft_strdup

--- src/ft_strdup.s
+++ src/ft_strdup.s
@@ -1,5 +1,25 @@
 global ft_strdup
+extern ft_strlen
+extern ft_strcpy
+extern malloc
 
 section .text
 	ft_strdup:
+        push rdi
+        call ft_strlen
+        inc rax
+
+        mov rdi, rax
+        call malloc wrt ..plt
+        test rax, rax
+        jz .error
+
+        mov rdi, rax
+        pop rsi
+        call ft_strcpy
+        ret
+
+	.error:
+        pop rdi
+		xor rax, rax
 		ret
