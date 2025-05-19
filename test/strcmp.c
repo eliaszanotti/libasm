ce63cece45cc35434da10946c8f57cefac88e19c
Elias ZANOTTI <zanotti.elias@gmail.com>
2025-05-19 11:15:34 +0200

    add some test

--- test/strcmp.c
+++ test/strcmp.c
@@ -4,7 +4,7 @@ static void _strcmp_print_result(const char *s1, const char *s2) {
     int ft_strcmp_result = ft_strcmp(s1, s2);
     int strcmp_result = strcmp(s1, s2);
 
-    printf("--- %s | %s ---\n", s1, s2);
+    printf("--- %s | %s ---\n", s1 ? s1 : "NULL", s2 ? s2 : "NULL");
     printf("ft_strcmp : %d\n", ft_strcmp_result);
     printf("strcmp    : %d\n", strcmp_result);
     if (ft_strcmp_result == strcmp_result) {
@@ -20,11 +20,31 @@ void test_strcmp() {
     _strcmp_print_result("Hello World!", "");
     _strcmp_print_result("Bonjour", "Bonjour");
     _strcmp_print_result("Bonjour", "Hello");
-    _strcmp_print_result("abc", "abc");
-    _strcmp_print_result("abc", "abcd");
-    _strcmp_print_result("abcd", "abc");
-    _strcmp_print_result("Hello", "hello");
-    _strcmp_print_result("123", "123");
-    _strcmp_print_result("123", "456");
-    _strcmp_print_result("ft_test", "ft_teat");
+    
+    _strcmp_print_result("Hello\nWorld", "Hello\nWorld");
+    _strcmp_print_result("Hello\tWorld", "Hello\tWorld");
+    _strcmp_print_result("Hello\0World", "Hello\0World");
+    
+    _strcmp_print_result("élias", "elias");
+    _strcmp_print_result("Hello 世界", "Hello World");
+    _strcmp_print_result("Hello 🌍", "Hello World");
+    
+    char long_str1[1000] = "A";
+    char long_str2[1000] = "A";
+    for (int i = 1; i < 999; i++) {
+        long_str1[i] = 'A';
+        long_str2[i] = 'A';
+    }
+    long_str1[999] = '\0';
+    long_str2[999] = '\0';
+    _strcmp_print_result(long_str1, long_str2);
+    
+    long_str2[998] = 'B';
+    _strcmp_print_result(long_str1, long_str2);
+
+    _strcmp_print_result("\x01\x02\x03", "\x01\x02\x03");
+    _strcmp_print_result("\x01\x02\x03", "\x01\x02\x04");
+    
+    _strcmp_print_result("\x7F\x80\x81", "\x7F\x80\x81");
+    _strcmp_print_result("\x7F\x80\x81", "\x7F\x80\x82");
 }
--- test/strdup.c
+++ test/strdup.c
@@ -4,12 +4,14 @@ static void _strdup_print_result(const char *src) {
     char *ft_strdup_result;
     char *strdup_result;
 
+    printf("--- %s ---\n", src ? src : "NULL");
+    
     ft_strdup_result = ft_strdup(src);
     strdup_result = strdup(src);
 
-    printf("--- %s ---\n", src);
-    printf("ft_strdup : %s\n", ft_strdup_result);
-    printf("strdup    : %s\n", strdup_result);
+    printf("ft_strdup : %s\n", ft_strdup_result ? ft_strdup_result : "NULL");
+    printf("strdup    : %s\n", strdup_result ? strdup_result : "NULL");
+    
     if ((ft_strdup_result == NULL && strdup_result == NULL) || 
         (ft_strdup_result != NULL && strdup_result != NULL && strcmp(ft_strdup_result, strdup_result) == 0)) {
         printf("\033[0;32mOK\033[0m\n\n");
@@ -22,18 +24,26 @@ static void _strdup_print_result(const char *src) {
 }
 
 void test_strdup(void) {
-    char *strs[] = {
-        "Hello, world!",
-        "",
-        "A",
-        "1234567890",
-        "This is a longer string to test the duplication function.",
-        "Special characters: !@#$%^&*()_+",
-        "こんにちは",
-        NULL
-    };
+    _strdup_print_result(NULL);
+    _strdup_print_result("");
+    _strdup_print_result("A");
+    _strdup_print_result("Hello\nWorld");
+    _strdup_print_result("Hello\tWorld");
+    _strdup_print_result("Hello\0World");
 
-    for (int i = 0; strs[i] != NULL; i++) {
-        _strdup_print_result(strs[i]);
+    _strdup_print_result("élias");
+    _strdup_print_result("Hello 世界");
+    _strdup_print_result("Hello 🌍");
+
+    char long_str[1000];
+    for (int i = 0; i < 999; i++) {
+        long_str[i] = 'A';
     }
+    long_str[999] = '\0';
+    _strdup_print_result(long_str);
+
+    _strdup_print_result("\x01\x02\x03");
+    _strdup_print_result("\x7F\x80\x81");
+    _strdup_print_result("Hello\n\t\r\0World");
+    _strdup_print_result("Hello 👨‍👩‍👧‍👦 World");
 }
