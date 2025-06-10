edb83f99c8d6f973f1b0d5406ce2a13c62bce7da
Elias ZANOTTI <zanotti.elias@gmail.com>
2025-06-10 10:56:57 +0200

    add some comment

--- src/ft_read.s
+++ src/ft_read.s
@@ -16,9 +16,13 @@ section .text
 		ret
 
 	.error:
+		;negate the value of rax (if syscall error -1, we need to return 1 in errno)
 		neg rax
 		mov r8, rax
+		;return the address of errno
 		call __errno_location wrt ..plt
+		;store the value of r8 in the address of errno ([rax] = errno address)
 		mov [rax], r8
+		;-1 is the return value for error
 		mov rax, -1
 		ret
